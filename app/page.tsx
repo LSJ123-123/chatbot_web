799f331271eab94d17e3da4dd48f33629a475515
Eihwan Shin <syehwany38@gmail.com>
2024-07-01 21:11:24 +0900

    supabase 설정
    supabase와 연결하기 위해 라이브러리 설치 및 코드 작성

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,53 @@
 
 import styles from "./page.module.css";
 import React from 'react';
+import { useState, useEffect } from "react";
+import { supabase } from '../supabase/client';
+
+interface test {
+  id: number;
+  text: string;
+}
 
 const FeedbackForm: React.FC = () => {
+  //supabase 연결 테스트 코드, 현재 빈 배열만 반환함
+  // const [test, setTest] = useState<test[]>([]);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const { data, error } = await supabase
+  //       .from('test')
+  //       .select('*')
+  //     if (data) {
+  //       setTest(data);
+  //       console.log(data);
+  //     }
+  //     if (error) {
+  //       console.error(error);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
+
   return (
     <div className={styles.container}>
-    <div className={styles.profile}>
-      <img src="/profile.png" alt="Profile" className={styles['profile-image']} />
-    </div>
-    <div className={styles.feedback}>
-      <div className={styles['thumbs-up']}>
-        <img src="/thumbs-up.png" alt="Thumbs Up" />
+      <div className={styles.profile}>
+        <img src="/profile.png" alt="Profile" className={styles['profile-image']} />
       </div>
-      <div className={styles['thumbs-down']}>
-        <img src="/thumbs-down.png" alt="Thumbs Down" />
+      <div className={styles.feedback}>
+        <div className={styles['thumbs-up']}>
+          <img src="/thumbs-up.png" alt="Thumbs Up" />
+        </div>
+        <div className={styles['thumbs-down']}>
+          <img src="/thumbs-down.png" alt="Thumbs Down" />
+        </div>
+      </div>
+      <div className={styles.message}>
+        <label htmlFor="message">메세지 소개</label>
+        <textarea id="message" placeholder="Type your message here"></textarea>
+        <p>Your message will be copied to the support team.</p>
       </div>
     </div>
-    <div className={styles.message}>
-      <label htmlFor="message">메세지 소개</label>
-      <textarea id="message" placeholder="Type your message here"></textarea>
-      <p>Your message will be copied to the support team.</p>
-    </div>
-  </div>
   );
 };
 
