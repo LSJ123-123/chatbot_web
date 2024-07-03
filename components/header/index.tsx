f25b74abb2a289a5a675535ef153b855cf34f011
Eihwan Shin <syehwany38@gmail.com>
2024-07-03 15:57:31 +0900

    Sidebar 기초 구조 추가, Zustand 상태 관리 도구 추가
    sidebar만 추가해서 현재 닫을수는 없음
    Zustand 라이브러리가 추가되었음

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,13 +5,15 @@ const linkStyle = {
   marginRight: 15
 };
 
-const Header = () => (
-  <div className="flex items-center justify-between p-6 bg-gray-800 text-white mb-8">
-    <div className="flex items-center">
+const Header = () => {
+
+  return (
+  <header className="flex items-center justify-between p-6 bg-gray-800 text-white mb-8">
+    <div className="flex items-center ml-4">
       <Link style={linkStyle} href="/">(로고) 시네마캐릭터</Link>
       <Link style={linkStyle} href="/login">login</Link>
     </div>
-    <div className="flex items-center">
+    <div className="flex items-center mr-4">
       <CommandDialogTrigger>
         <CommandInput placeholder="Type a command or search..." />
         <CommandList>
@@ -24,7 +26,7 @@ const Header = () => (
         </CommandList>
       </CommandDialogTrigger>
     </div>
-  </div>
-);
+  </header>
+)};
 
 export default Header;
---
+++ components/sidebar/index.tsx
@@ -0,0 +1,17 @@
+const Sidebar = () => {
+    return (
+    <aside className="sidebar">
+        <nav>
+        <ul>
+            <li>
+            <a href="/about">About</a>
+            </li>
+            <li>
+            <a href="/contact">Contact</a>
+            </li>
+        </ul>
+        </nav>
+    </aside>
+)};
+
+export default Sidebar;
