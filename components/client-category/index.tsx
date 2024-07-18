c3c7d384ef6ef8da60c2245bcb0678e9b8356b5d
LSJ123-123 <113840393+LSJ123-123@users.noreply.github.com>
2024-07-18 14:31:53 +0900

    Carousel 버튼 위치 수정
    Carousel 버튼 위치를 수정하기 위헤 기존 div에 묶여있는 Carousel를 바깥으로 빼서 챗봇 카테고리 전체가 Carousel로 인식되게 바꿈

--- components/client-category/index.tsx
+++ components/client-category/index.tsx
@@ -20,7 +20,13 @@ const ClientCategorySection = ({ initialCategories, initialChatbots }: { initial
         : initialChatbots;
 
     return (
-        <div className="w-full max-w-screen container mx-auto p-5">
+        <Carousel
+            opts={{
+                align: "start",
+            }}
+            className="w-full max-w-screen container mx-auto p-5"
+        >
+
             <p className="text-2xl font-bold mb-2">챗봇 카테고리</p>
             <div className="flex overflow-x-auto space-x-2 mb-4">
                 {initialCategories.map((category: any) => (
@@ -33,31 +39,25 @@ const ClientCategorySection = ({ initialCategories, initialChatbots }: { initial
                     </Button>
                 ))}
             </div>
-            <Carousel
-                opts={{
-                    align: "start",
-                }}
-                className="w-full"
-            >
-                <CarouselContent className="flex">
-                    {filteredChatbots.map((chatbot: any) => (
-                        <CarouselItem key={chatbot.id} className="md:basis-1/2 lg:basis-1/3">
-                            <div className="p-1">
-                                <ChatList
-                                    img={chatbot.img}
-                                    name={chatbot.name}
-                                    desc={chatbot.chatbot_desc}
-                                    link={chatbot.id}
-                                    category={selectedCategory ? selectedCategory : '0'}
-                                />
-                            </div>
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-                <CarouselPrevious className="bg-gray-200 p-2 rounded-full absolute top-1/2 left-4 transform -translate-y-1/2 -translate-x-1/2" />
-                <CarouselNext className="bg-gray-200 p-2 rounded-full absolute top-1/2 right-4 transform -translate-y-1/2 translate-x-1/2" />
-            </Carousel>
-        </div>
+
+            <CarouselContent className="flex">
+                {filteredChatbots.map((chatbot: any) => (
+                    <CarouselItem key={chatbot.id} className="md:basis-1/2 lg:basis-1/3">
+                        <div className="p-1">
+                            <ChatList
+                                img={chatbot.img}
+                                name={chatbot.name}
+                                desc={chatbot.chatbot_desc}
+                                link={chatbot.id}
+                                category={selectedCategory ? selectedCategory : '0'}
+                            />
+                        </div>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious className="bg-gray-200 p-2 rounded-full absolute top-[62.5%] left-4 transform -translate-y-1/2 -translate-x-1/2" />
+            <CarouselNext className="bg-gray-200 p-2 rounded-full absolute top-[62.5%] right-4 transform -translate-y-1/2 translate-x-1/2" />
+        </Carousel>
     );
 };
 
